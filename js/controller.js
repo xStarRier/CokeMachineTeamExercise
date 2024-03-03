01785329652a688d320c1f523141353902e6c086
GET Academy <terje@getacademy.no>
2024-03-03 19:15:40 +0100

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -1,43 +1,11 @@
 function buyCoke() {
-    let valueInserted = valueFromCoinCounts(coinsInserted);
-    let valueToReturn = valueInserted - 25;
-    if (valueInserted < 25) {
-        errorMessage = 'Ikke nok penger';
-        return;
-    }
-    if (!giveChangeAndReturnIsSuccess(false, valueToReturn)) {
-        errorMessage = 'Automaten har ikke returmynter';
-        return;
-    }
-    giveChangeAndReturnIsSuccess(true, valueToReturn);
     cokesInStore--;
     isCokeInDelivery = true;
     updateView();
 }
 
-function giveChangeAndReturnIsSuccess(actuallyDoIt, amount) {
-    for (let i = coinsInMachine.length - 1; i >= 0; i--) {
-        let coinValue = coinValueFromIndex(i);
-        let coinCountNeeded = Math.floor(amount / coinValue);
-        let coinCountInMachine = coinsInMachine[i];
-        let coinCount = coinCountInMachine < coinCountNeeded ? coinCountInMachine : coinCountNeeded;
-        amount -= coinValue * coinCount;
-        if (actuallyDoIt) {
-            coinsReturned[i] += coinCount;
-            coinsInMachine[i] -= coinCount;
-        }
-    }
-    return amount == 0;
-}
-
 function insertCoin(value) {
-    const index =
-        value == 1 ? 0 :
-            value == 5 ? 1 :
-                value == 10 ? 2 :
-                    value == 20 ? 3 :
-                        null;
-    coinsInserted[index]++;
+    coinsInserted[0]++;
     updateView();
 }
 
